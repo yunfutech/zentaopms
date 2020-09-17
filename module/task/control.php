a8b6fb6aec3580971c60cd46079dfe5863d0b543
zhouhao <zhouhao@yunfutech.com>
2020-09-17 09:29:50 +0800

    去掉取消和关闭的迭代

--- module/task/control.php
+++ module/task/control.php
@@ -1597,7 +1597,7 @@ class task extends control
                     array_push($moreUsers, ['name' => $user->realname, 'estimate' => $sum]);
                 }
 
-                $delayTasks = $this->dao->select('count(t1.id) as cnt')->from(TABLE_TASK)->alias('t1')->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.project = t2.id')->where('t1.status')->ne('closed')->andWhere('t1.status')->ne('cancel')->andWhere('t1.status')->ne('done')->andWhere('t1.deadline')->lt($today)->andWhere('t1.assignedTo')->eq($user->account)->andWhere('t1.deleted')->ne(1)->fetch();
+                $delayTasks = $this->dao->select('count(t1.id) as cnt')->from(TABLE_TASK)->alias('t1')->leftJoin(TABLE_PROJECT)->alias('t2')->on('t1.project = t2.id')->where('t1.status')->ne('closed')->andWhere('t1.status')->ne('cancel')->andWhere('t1.status')->ne('done')->andWhere('t1.deadline')->lt($today)->andWhere('t1.assignedTo')->eq($user->account)->andWhere('t2.status')->notin('cancel, closed')->andWhere('t1.deleted')->ne(1)->fetch();
                 // var_dump($this->dao->sqlobj);
                 if ($delayTasks->cnt == 0) {
                     continue;
