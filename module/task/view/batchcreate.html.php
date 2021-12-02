bd616350261c7ab76d549cb770b554bd7402c556
zhouhao <zhouhao@yunfutech.com>
2021-12-02 20:23:48 +0800

    修改文案

--- module/task/view/batchcreate.html.php
+++ module/task/view/batchcreate.html.php
@@ -34,7 +34,7 @@
     </div>
     <div class="pull-right text-danger" style="padding: 6px;margin-right: 30px;">
       <i class="icon icon-exclamation-sign"></i>
-      <span>注意: 默认截止日期在每天<?php echo $config->task->deadlinePoint?>点之后为明天</span>
+      <span>注意: 默认截止日期在<?php echo $config->task->deadlinePoint?>点之后为明天</span>
     </div>
   </div>
   <?php
