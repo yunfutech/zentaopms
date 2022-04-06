6f77e38800ba921bced97645dd3810e3ea8b0f1a
zhouhao <zhouhao@yunfutech.com>
2022-04-06 14:15:53 +0800

    创建项目白名单增加测试主管

--- module/product/view/create.html.php
+++ module/product/view/create.html.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The create view of product module of ZenTaoPMS.
  *
@@ -10,114 +11,124 @@
  * @link        http://www.zentao.net
  */
 ?>
-<?php include '../../common/view/header.html.php';?>
-<?php include '../../common/view/kindeditor.html.php';?>
-<?php js::set('noProject', ($config->global->flow == 'onlyStory' or $config->global->flow == 'onlyTest') ? true : false);?>
+<?php include '../../common/view/header.html.php'; ?>
+<?php include '../../common/view/kindeditor.html.php'; ?>
+<?php js::set('noProject', ($config->global->flow == 'onlyStory' or $config->global->flow == 'onlyTest') ? true : false); ?>
 <div id="mainContent" class="main-content">
   <div class="center-block">
     <div class="main-header">
-      <h2><?php echo $lang->product->create;?></h2>
+      <h2><?php echo $lang->product->create; ?></h2>
     </div>
     <form class="load-indicator main-form form-ajax" id="createForm" method="post" target='hiddenwin'>
       <table class="table table-form">
         <tbody>
           <tr>
-            <th class='w-140px'><?php echo $lang->product->name;?></th>
-            <td><?php echo html::input('name', '', "class='form-control input-product-title' required");?></td><td></td>
+            <th class='w-140px'><?php echo $lang->product->name; ?></th>
+            <td><?php echo html::input('name', '', "class='form-control input-product-title' required"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->pri;?></th>
+            <th><?php echo $lang->product->pri; ?></th>
             <td colspan='1'>
-            <?php
+              <?php
               $priList = $lang->product->priList;
-            ?>
-            <?php echo html::select('pri', (array)$priList, '3', "class='form-control'");?>
+              ?>
+              <?php echo html::select('pri', (array)$priList, '3', "class='form-control'"); ?>
             </td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->code;?></th>
-            <td><?php echo html::input('code', '', "class='form-control input-product-code' required");?></td><td></td>
+            <th><?php echo $lang->product->code; ?></th>
+            <td><?php echo html::input('code', '', "class='form-control input-product-code' required"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->line;?></th>
+            <th><?php echo $lang->product->line; ?></th>
             <td>
               <div class='input-group' id='lineIdBox'>
-                <?php echo html::select('line', $lines, '', "class='form-control chosen'");?>
-                <span class='input-group-addon'><?php echo html::a($this->createLink('tree', 'browse', "rootID=$rootID&view=line", '', true), $lang->tree->manageLine, '', "class='text-primary' data-toggle='modal' data-type='iframe' data-width='95%'");?></span>
+                <?php echo html::select('line', $lines, '', "class='form-control chosen'"); ?>
+                <span class='input-group-addon'><?php echo html::a($this->createLink('tree', 'browse', "rootID=$rootID&view=line", '', true), $lang->tree->manageLine, '', "class='text-primary' data-toggle='modal' data-type='iframe' data-width='95%'"); ?></span>
               </div>
-              <div class='hidden'><?php echo html::a("javascript:void(0)", $lang->refresh, '', "class='refresh' onclick='loadProductLines($rootID)'");?></div>
+              <div class='hidden'><?php echo html::a("javascript:void(0)", $lang->refresh, '', "class='refresh' onclick='loadProductLines($rootID)'"); ?></div>
             </td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->director;?></th>
-            <td><?php echo html::select('director', $poUsers, '', "class='form-control chosen'");?></td><td></td>
+            <th><?php echo $lang->product->director; ?></th>
+            <td><?php echo html::select('director', $poUsers, '', "class='form-control chosen'"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->counselor;?></th>
-            <td><?php echo html::select('counselor', $poUsers, '', "class='form-control chosen'");?></td><td></td>
+            <th><?php echo $lang->product->counselor; ?></th>
+            <td><?php echo html::select('counselor', $poUsers, '', "class='form-control chosen'"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->PO;?></th>
-            <td><?php echo html::select('PO', $poUsers, '', "class='form-control chosen'");?></td><td></td>
+            <th><?php echo $lang->product->PO; ?></th>
+            <td><?php echo html::select('PO', $poUsers, '', "class='form-control chosen'"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->QD;?></th>
-            <td><?php echo html::select('QD', $qdUsers, '', "class='form-control chosen'");?></td><td></td>
+            <th><?php echo $lang->product->QD; ?></th>
+            <td><?php echo html::select('QD', $qdUsers, '', "class='form-control chosen'"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->RD;?></th>
-            <td><?php echo html::select('RD', $rdUsers, '', "class='form-control chosen'");?></td><td></td>
+            <th><?php echo $lang->product->RD; ?></th>
+            <td><?php echo html::select('RD', $rdUsers, '', "class='form-control chosen'"); ?></td>
+            <td></td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->type;?></th>
+            <th><?php echo $lang->product->type; ?></th>
             <td>
               <?php
               $proudctTypeList = array();
-              foreach($lang->product->typeList as $key => $type) $productTypeList[$key] = $type . zget($lang->product->typeTips, $key, '');
+              foreach ($lang->product->typeList as $key => $type) $productTypeList[$key] = $type . zget($lang->product->typeTips, $key, '');
               ?>
-              <?php echo html::select('type', $productTypeList, 'normal', "class='form-control'");?>
-            </td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->product->progress;?></th>
-            <td>
-              <div class='input-group'>
-                <?php echo html::input('progress', '', "class='form-control' placeholder='". $lang->product->progressPlaceholder ."'");?>
-                <span class="input-group-addon">%</span>
-              </div>
-            </td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->product->state;?></th>
-            <td>
-              <?php echo html::select('state', $lang->product->stateList, '', "class='form-control chosen'");?>
-            </td><td></td>
-          </tr>
-          <tr class='hide'>
-            <th><?php echo $lang->product->status;?></th>
-            <td><?php echo html::hidden('status', 'normal');?></td>
+              <?php echo html::select('type', $productTypeList, 'normal', "class='form-control'"); ?>
+            </td>
             <td></td>
           </tr>
-          <?php $this->printExtendFields('', 'table');?>
           <tr>
-            <th><?php echo $lang->product->desc;?></th>
+            <th><?php echo $lang->product->progress; ?></th>
+            <td>
+              <div class='input-group'>
+                <?php echo html::input('progress', '', "class='form-control' placeholder='" . $lang->product->progressPlaceholder . "'"); ?>
+                <span class="input-group-addon">%</span>
+              </div>
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <th><?php echo $lang->product->state; ?></th>
+            <td>
+              <?php echo html::select('state', $lang->product->stateList, '', "class='form-control chosen'"); ?>
+            </td>
+            <td></td>
+          </tr>
+          <tr class='hide'>
+            <th><?php echo $lang->product->status; ?></th>
+            <td><?php echo html::hidden('status', 'normal'); ?></td>
+            <td></td>
+          </tr>
+          <?php $this->printExtendFields('', 'table'); ?>
+          <tr>
+            <th><?php echo $lang->product->desc; ?></th>
             <td colspan='2'>
-              <?php echo $this->fetch('user', 'ajaxPrintTemplates', "type=product&link=desc");?>
-              <?php echo html::textarea('desc', '', "rows='8' class='form-control kindeditor' hidefocus='true' tabindex=''");?>
+              <?php echo $this->fetch('user', 'ajaxPrintTemplates', "type=product&link=desc"); ?>
+              <?php echo html::textarea('desc', '', "rows='8' class='form-control kindeditor' hidefocus='true' tabindex=''"); ?>
             </td>
           </tr>
           <tr>
-            <th><?php echo $lang->product->acl;?></th>
-            <td colspan='2'><?php echo nl2br(html::radio('acl', $lang->product->aclList, 'custom', "onclick='setWhite(this.value);'", 'block'));?></td>
+            <th><?php echo $lang->product->acl; ?></th>
+            <td colspan='2'><?php echo nl2br(html::radio('acl', $lang->product->aclList, 'custom', "onclick='setWhite(this.value);'", 'block')); ?></td>
           </tr>
           <tr id='whitelistBox'>
-            <th><?php echo $lang->product->whitelist;?></th>
-            <td colspan='2'><?php echo html::checkbox('whitelist', $groups, '1,4', '', 'inline');?></td>
+            <th><?php echo $lang->product->whitelist; ?></th>
+            <td colspan='2'><?php echo html::checkbox('whitelist', $groups, '1,4,8', '', 'inline'); ?></td>
           </tr>
           <tr>
             <td colspan='3' class='text-center form-actions'>
-              <?php echo html::submitButton();?>
-              <?php echo html::backButton();?>
+              <?php echo html::submitButton(); ?>
+              <?php echo html::backButton(); ?>
             </td>
           </tr>
         </tbody>
@@ -125,4 +136,4 @@
     </form>
   </div>
 </div>
-<?php include '../../common/view/footer.html.php';?>
+<?php include '../../common/view/footer.html.php'; ?>
